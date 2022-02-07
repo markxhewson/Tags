7cad0b56c47806a25aafdf31f93e4a7775b44411
Lotho <58112436+MLotho@users.noreply.github.com>
2022-02-07 10:34:26 +0000

    some bug fixes for paginated menu

--- src/main/java/net/lotho/interfaces/TagsMenu.java
+++ src/main/java/net/lotho/interfaces/TagsMenu.java
@@ -46,9 +46,9 @@ public class TagsMenu {
         this.maxPages = playerTags.size() / this.maxItemsPerPage;
 
         this.inventory.setItem(45, GUI.createItem(Material.ARROW, "&7&lBack", Chat.color("&7Right click to go back to the tags menu.")));
-        this.inventory.setItem(48, GUI.createItem(Material.BIRCH_DOOR, "&aPrevious Page", Chat.color("&7Click to go to the previous page")));
+        this.inventory.setItem(48, GUI.createItem(Material.FENCE_GATE, "&aPrevious Page", Chat.color("&7Click to go to the previous page")));
         this.inventory.setItem(49, GUI.createItem(Material.BLAZE_ROD, "&a&lPage", Chat.color("&7You are on page " + (this.page + 1) + "/" + (this.maxPages + 1))));
-        this.inventory.setItem(50, GUI.createItem(Material.BIRCH_DOOR, "&aNext Page", Chat.color("&7Click to go to the next page")));
+        this.inventory.setItem(50, GUI.createItem(Material.FENCE_GATE, "&aNext Page", Chat.color("&7Click to go to the next page")));
 
         for (int i = 0; i < this.inventory.getSize(); i++) {
             if (i < 9 && this.inventory.getItem(i) == null) this.inventory.setItem(i, GUI.createItemShort(Material.STAINED_GLASS_PANE, 7, " "));
@@ -84,13 +84,17 @@ public class TagsMenu {
                 }
                 break;
 
-            case BIRCH_DOOR:
+            case FENCE_GATE:
                 if (ChatColor.stripColor(clickedItem.getItemMeta().getDisplayName()).contains("Next Page")) {
-                    this.page += 1;
-                    open(clicker);
+                    if (this.page >= this.maxPages) {
+                        clicker.sendMessage(Chat.color("&c&lError! &7You are already on the last page."));
+                    } else {
+                        this.page += 1;
+                        open(clicker);
+                    }
                 }
                 else if (ChatColor.stripColor(clickedItem.getItemMeta().getDisplayName()).contains("Previous Page")) {
-                    if (this.page >= 1) {
+                    if (this.page <= 2) {
                         clicker.sendMessage(Chat.color("&c&lError! &7You are already on the first page!"));
                     } else {
                         this.page -= 1;
